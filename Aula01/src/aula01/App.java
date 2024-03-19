e78f5075d4be3980cfff8a460d408c0cec2e1c96
João Paulo Barros <108131013+JooJPaulo@users.noreply.github.com>
2024-03-18 21:07:17 -0300

    Add files via upload

---
+++ Aula01/src/aula01/App.java
@@ -0,0 +1,16 @@
+package aula01;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+
+		Ola ola1 = new Ola();
+		Tchau tchau1 = new Tchau();
+		
+		ola1.start();
+		tchau1.start();
+	}
+
+}
---
+++ Aula01/src/aula01/Tchau.java
@@ -0,0 +1,9 @@
+package aula01;
+
+public class Tchau extends Thread{
+	public void run() {
+		System.out.println("Entrando em: " + this.getName());
+		System.out.println(this.getName() + " Tchau!");
+		System.out.println("Saindo da: " + this.getName());
+	}
+}
